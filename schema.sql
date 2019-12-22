d8714e1a66cb0a0647f5855231648ce3c857f6af
Ellen <evwhite2@gmail.com>
2019-12-22 17:08:21 -0600

    heroku commit

--- schema.sql
+++ schema.sql
@@ -1,11 +1,11 @@
-DROP DATABASE IF EXISTS devour_db;
-CREATE DATABASE devour_db;
-USE devour_db;
+-- DROP DATABASE IF EXISTS devour_db;
+-- CREATE DATABASE devour_db;
+-- USE devour_db;
 
-CREATE TABLE meals(
-	id INT NOT NULL auto_increment,
-    txt VARCHAR(150) NOT NULL,
-    devoured BOOLEAN
-		DEFAULT false,
-    PRIMARY KEY(id)
-);
+-- CREATE TABLE meals(
+-- 	id INT NOT NULL auto_increment,
+--     txt VARCHAR(150) NOT NULL,
+--     devoured BOOLEAN
+-- 		DEFAULT false,
+--     PRIMARY KEY(id)
+-- );
